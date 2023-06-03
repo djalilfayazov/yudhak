369550e82e12a7a92dd1db93a4f4591d0f760f59
djalilfayazov <djalilfayazov@gmail.com>
2023-06-03 18:58:14 +0500

    готовая маршрутизация

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,13 @@ from .views import *
 
 
 urlpatterns = [
-	path('', index)
+	path('', index, name='index'),
+
+	path('about/', about, name='about'),
+	path('contact/', contact, name='contact'),
+	path('course-detail/', course_detail, name='course-detail'),
+	path('courses/', courses, name='courses'),
+	path('events/', events, name='events'),
+	path('pricing/', pricing, name='pricing'),
+	path('trainers/', trainers, name='trainers'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,5 +3,40 @@ from django.shortcuts import render
 
 def index(request):
 	return render(
-		request, 'course-details.html'
+		request, 'index.html'
+	)
+
+def about(request):
+	return render(
+		request, 'about.html'
+	)
+
+def contact(request):
+	return render(
+		request, 'contact.html'
+	)
+
+def courses(request):
+	return render(
+		request, 'courses.html'
+	)
+
+def events(request):
+	return render(
+		request, 'events.html'
+	)
+
+def pricing(request):
+	return render(
+		request, 'pricing.html'
+	)
+
+def trainers(request):
+	return render(
+		request, 'trainers.html'
+	)
+
+def course_detail(request):
+	return render(
+		request, 'course-detail.html'
 	)
